16df4760b236ce12c845bb0b16aee0d161805e10
Marcos Vinicius <85797701+strngrthnall@users.noreply.github.com>
2023-09-29 18:37:04 -0300

    qualquer coisa

---
+++ diretivas-proj/src/app/card/card.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit {
+  produtos: string[] = []
+
+  constructor() {
+    this.produtos = [
+      'mouse',
+      'teclado',
+      'cabo',
+      'fonte',
+      'placa de vídeo'
+    ]
+  }
+
+  ngOnInit(): void {
+  }
+
+}
